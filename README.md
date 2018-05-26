4f11a014f4fbef75b14e466fb1c59be78c86a958
David <dk.shin1992@gmail.com>
2018-05-26 10:32:12 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Bell experiment laser characterisation
+
+## 1. Intensity stabilisation
+
+## 2. Beam position stability
+
+## 3. Beam profile - fringe cancelled
+
---
+++ main_intensity_stab.m
@@ -0,0 +1,41 @@
+% Long-term monitoring of optical power in Raman laser system
+% DKS
+% 2018-05-24
+
+
+
+%%% config
+data_file='C:\Users\HE BEC\Documents\lab\bell_momentumspin\laser\intensity_stab\exp1_20180523\raw\tek40xx\meas\imes0001.csv';
+
+
+
+%% main
+dd=read_DMM_log(data_file);
+
+
+%%% get data
+t=dd(:,1);      % time (s)
+V=dd(:,2);      % photodetector output voltage (V)
+v=V/mean(V);   % normalised by the mean value
+
+
+%%% statistics
+v_std=std(v)       % relative uncertainty of intensity
+
+
+
+%% visualise
+figure('Name','Vt');
+plot(t,v,'.','DisplayName','data');
+
+xlabel('t [s]');
+ylabel('Beam Intensity (arb. unit)');
+
+vf=smooth(v,100);
+
+hold on;
+plot(t,vf,'-','DisplayName','smoothed','LineWidth',2);
+
+legend('show');
+
+
